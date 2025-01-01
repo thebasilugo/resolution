1b94deb20d099dab1d2f8ddb3d7377d12ae4db3c
Basil Ugo <basilugo2@gmail.com>
2025-01-01 22:07:47 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+	reactStrictMode: true,
+	swcMinify: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
